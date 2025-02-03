edeac804299293f296b8966b45be23159c856e1f
dinesh13p <159114512+dinesh13p@users.noreply.github.com>
2025-02-03 20:52:03 +0545

    Added some new files

---
+++ Basics/Assignment2.java
@@ -0,0 +1,57 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+
+// To check whether a person is eligible to vote or not:
+        
+        int age = 23;
+        if (age >= 18) {
+            System.out.println("Eligible to vote!");
+        }
+        else {
+            System.out.println("Is not eligible to vote!");
+        }
+
+// Simple calculator:
+        // System.out.println("Calculator: ");
+        // float a = 12;
+        // float b = 13;
+
+        // // Operation to perform (+, -, *, /)
+        // char operation = '*';
+
+        // float result = 0;
+
+        // switch (operation) {
+        //     case '+':
+        //         result = a + b;
+        //         break;
+        //     case '-':
+        //         result = a - b;
+        //         break;
+        //     case '*':
+        //         result = a * b;
+        //         break;
+        //     case '/':
+        //         if (b != 0) {
+        //             result = a / b;
+        //         } else {
+        //             System.out.println("Error: Division by zero!");
+        //             return;
+        //         }
+        //         break;
+        //     default:
+        //         System.out.println("Invalid operation!");
+        //         return;
+        // }
+        // System.out.println("Result: " + result);
+
+// Multiplication table of 5:
+        System.out.println("Multiplication table of 5: ");
+        int number = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+
+    }
+    
---
+++ Practice/Questions.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class Questions {
+    public static void main(String[] args) {
+
+        // To check whether a person is eligible to vote or not
+        int age = 23;
+        if (age >= 18) {
+            System.out.println("Eligible to vote!");
+        }
+        else {
+            System.out.println("Is not eligible to vote!");
+        }
+
+
+    }
+    
+}
