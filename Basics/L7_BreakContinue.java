a77de1100130f3e3beedd2881b574603c887f9f5
dinesh13p <159114512+dinesh13p@users.noreply.github.com>
2025-02-11 11:34:18 +0545

    commit

--- Basics/L7_BreakContinue.java
+++ Basics/L7_BreakContinue.java
@@ -5,7 +5,7 @@ public class L7_BreakContinue {
         int i;
         for (i = 0; i <= 10; i++){
 
-            if (i == 5){
+            if (i == 4){
                 break;
             }
             System.out.println(i);
